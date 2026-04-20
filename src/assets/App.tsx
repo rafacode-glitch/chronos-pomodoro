7148f3d9ea21dc47489ceebcf50844ba7e58952b
Rafael Lourival <rafaellourivaldasilva@gmail.com>
2026-04-20 18:15:39 -0300

    uns bagui ai denovo 4

--- src/assets/App.tsx
+++ src/assets/App.tsx
@@ -1,2 +1,19 @@
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <Heading attr={123} attr2='String'>
+        Olá mundo 1
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ex
+        ducimus quae, iusto provident mollitia ad rerum, id ut, at magnam
+        doloremque quas. Velit accusamus beatae repellendus a perspiciatis hic!
+      </p>
+    </>
+  );
+}
