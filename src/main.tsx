7173a62ee2dccec09f9cebf500327ffb246544a1
Rafael Lourival <rafaellourivaldasilva@gmail.com>
2026-04-19 21:43:30 -0300

    uns bagui ai

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <h1>Olá Mundo!</h1>
   </StrictMode>,
-)
+);
