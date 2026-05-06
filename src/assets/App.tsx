082c0ab814216e045b2ac4fde1d2439d6c7b803c
Rafael Lourival <rafaellourivaldasilva@gmail.com>
2026-05-06 14:59:51 -0300

    commit 15h

--- src/assets/App.tsx
+++ src/assets/App.tsx
@@ -3,12 +3,13 @@ import './styles/global.css';
 
 import { Heading } from '../components/Heading';
 import { Container } from '../components/Container';
+import { Logo } from '../components/Logo';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
