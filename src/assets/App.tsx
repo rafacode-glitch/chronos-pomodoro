299de97d6ca1c71746577908acc6ed3aaf6129bb
Rafael Lourival <rafaellourivaldasilva@gmail.com>
2026-05-07 16:57:38 -0300

    antes da net cair

--- src/assets/App.tsx
+++ src/assets/App.tsx
@@ -1,9 +1,10 @@
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Heading } from '../components/Heading';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
+import { Menu } from '../components/Menu';
+import { CountDown } from '../components/CountDown';
 
 export function App() {
   return (
@@ -13,15 +14,11 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
 
       <Container>
-        <Heading>FORM</Heading>
-      </Container>
-
-      <Container>
-        <Heading>FOOTER</Heading>
+        <CountDown />
       </Container>
     </>
   );
